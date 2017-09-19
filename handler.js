bb2e94cb9895ed377533e53ea6cdcf6f93a3f982
jamescross91 <james_cross91@hotmail.com>
2017-09-19 21:22:57 +0100

    adding endpoints for S3 and hello

--- handler.js
+++ handler.js
@@ -1,15 +1,32 @@
 'use strict';
+const aws = require('aws-sdk');
+const s3 = new aws.S3();
 
 module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      message: 'Hello' + event.pathParameters.name + 'youve setup a serverless API',
     }),
   };
-
   callback(null, response);
+};
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+module.exports.reads3 = (event, context, callback) => {
+  const getParams = {
+      Bucket: 'packt-james-test-bucket', // your bucket name,
+      Key: 'packt-test-file.txt' // path to the object you're looking for
+  }
+
+  s3.getObject(getParams, function(err, data) {
+
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        s3data: data.Body.toString('utf-8'),
+      }),
+    };
+
+    callback(null, response)
+  });
 };
